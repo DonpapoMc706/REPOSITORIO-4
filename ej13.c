6cc5526824a4facb5384d4603987877f8da6f6d2
DonpapoMc706 <pianarosa.zaid.et21.25@gmail.com>
2026-06-10 23:01:20 +0000

    Agregando ejercicios 13 y 14

---
+++ ej13.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    float tuVariable;
+    printf("ingresa un numero decimal: ");
+    scanf("%f", &tuVariable);
+    printf("el valor ingresado es: %.2f", tuVariable);
+    return 0;
+}
